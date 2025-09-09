a104c5b6d368773854c183abc8e66f00dc29019e
Jim <Jimschel1@outlook.com>
2025-09-09 11:22:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+	function showTime() {
+    document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
 showTime();
 setInterval(function () {
-	showTime();
+    showTime();
 }, 1000);
+function normalizeInput(input) {
+    // Zet alles om naar lowercase en verwijder ! ? .
+    return input
+        .toLowerCase()
+        .replace(/[!?.]/g, "");
+}
